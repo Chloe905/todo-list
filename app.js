85f237cdac41286944ac882d334a99d49a270c74
Chloe905 <cchloehsieh@gmail.com>
2022-11-21 12:54:11 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const router = require('./routes') // 引用路由器，路徑不用再加上 /index，因為express會自動去找 router下叫index的檔案
 require('./config/mongoose')
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
 const app = express()
-// 僅在非正式環境時，使用dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -20,6 +17,6 @@ app.use(methodOverride('_method'))
 // 導入路由器
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`This is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`This is running on http://localhost:${PORT}`)
 })
